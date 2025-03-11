b788658684dbfdb0146675683ab6897552c506b3
Netsuretsu <constantin-joval.awanchede@epitech.eu>
2025-03-11 21:47:29 +0100

    minor update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div className='flex items-center justify-center'>
-      <h2 className="font-bold text-4xl text-foodvision-primary">Foodvision</h2>
-      
+      <h2 className="font-bold text-4xl text-foodvision-primary uppercase">Foodvision</h2>
     </div>
   );
 }
